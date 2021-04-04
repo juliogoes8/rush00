46c5df67eef99db763c8aad9b10c81a04785c676
lufelipe <lufelipe@student.42sp.org.br>
2021-04-04 22:34:11 +0000

    Demais rush's

---
+++ rush00.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush00.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lufelipe <lufelipe@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/04 22:24:36 by lufelipe          #+#    #+#             */
+/*   Updated: 2021/04/04 22:26:58 by lufelipe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	rush(int x, int y)
+{
+	int i;
+	int j;
+
+	if (x == 0 || y == 0)
+		return ;
+	i = 1;
+	while (i <= y)
+	{
+		j = 1;
+		while (j <= x)
+		{
+			if ((i == 1 && j == 1) || (i == y && j == x) || (i == 1 && j == x) || (i == y && j == 1))
+				ft_putchar('o');
+			else if (i == 1 || i == y)
+				ft_putchar('-');
+			else if (j == 1 || j == x)
+				ft_putchar('|');
+			else
+				ft_putchar(' ');
+			j++;
+		}
+		ft_putchar('\n');
+		i++;
+	}
+}
